a825a147e424f4e966f500a253a82462bce021a3
Jens Schauder <jens@schauderhaft.de>
2025-12-12 13:41:54 +0100

    Migrate to sequence.

--- src/main/java/de/schauderhaft/kotlinsequence/Person.java
+++ src/main/java/de/schauderhaft/kotlinsequence/Person.java
@@ -1,6 +1,11 @@
 package de.schauderhaft.kotlinsequence;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Sequence;
 
-record Person(@Id Long id, String name) {
+record Person(
+		@Id
+		@Sequence(sequence = "ENTITY_SEQUENCE")
+		Long id,
+		String name) {
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-create table Person
-(
-    id   bigint generated always as identity primary key,
+create sequence entity_sequence start with 1 increment by 1;
+
+create table Person (
+    id bigint primary key,
     name varchar(200)
-);
+)
